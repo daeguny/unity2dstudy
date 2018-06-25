263a6473e366534cef5003d02e8eb0db00154e77
daeguny <whiteagl1004@gmail.com>
2018-06-26 08:07:33 +0900

    유니티 2D 게임공작소 챕터 4,5 분량 진행 중 메카님 애니메이션 제작 스크립트까지 작성

---
+++ Sample4/Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour {
+    PlayerController playerCtrl;
+
+    private void Awake()
+    {
+        playerCtrl = GetComponent<PlayerController>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!playerCtrl.activeSts)
+        {
+            return;
+        }
+
+        // 패드 처리
+        float joyMv = Input.GetAxis("Horizontal");
+        playerCtrl.ActionMove(joyMv);
+
+        // 점프
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerCtrl.ActionJump();
+        }
+	}
+}
